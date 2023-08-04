ab637fd6ed1a4e87fbd4ae1cff32b50ae630dd47
Thomson <thomson.martin@eminds.ai>
2023-08-04 18:47:47 +0530

    Formatting changes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-import WebGL from 'three/addons/capabilities/WebGL.js';
-import * as dat from 'dat.gui';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import WebGL from "three/addons/capabilities/WebGL.js";
+import * as dat from "dat.gui";
 
-if ( !WebGL.isWebGLAvailable() ) {
-	alert("WebGL is unsupported, animations would break")
+if (!WebGL.isWebGLAvailable()) {
+  alert("WebGL is unsupported, animations would break");
 }
 
 const scene = new THREE.Scene();
@@ -15,12 +15,11 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const orbit = new OrbitControls( camera, renderer.domElement);
+const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.autoRotate = true;
 
 const axesHelper = new THREE.AxesHelper(5);
@@ -30,17 +29,18 @@ camera.position.z = 5;
 camera.position.y = 2;
 orbit.update();
 
-
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: 0x00ff00});
+  color: 0x00ff00,
+});
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cube);
 
-
-
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide});
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
@@ -49,7 +49,10 @@ const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
 const sphereGeometry = new THREE.SphereGeometry(4, 40, 40);
-const sphereMaterial = new THREE.MeshBasicMaterial({color: 0x0000FF, wireframe: false});
+const sphereMaterial = new THREE.MeshBasicMaterial({
+  color: 0x0000ff,
+  wireframe: false,
+});
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
@@ -58,12 +61,12 @@ sphere.position.x = -10;
 const gui = new dat.GUI();
 
 const options = {
-	sphereColor: '#ffea00'
-}
+  sphereColor: "#ffea00",
+};
 
-gui.addColor(options, 'sphereColor').onChange(e => {
-	sphere.material.color.set(e)
-})
+gui.addColor(options, "sphereColor").onChange((e) => {
+  sphere.material.color.set(e);
+});
 
 // Three JS Line
 
